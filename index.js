15d1388c507daae800d42b7835bc89c9300d2f12
Dieter Kribbel <dieter.kribbel@gmx.at>
2020-09-03 07:09:40 +0200

    03090709

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emojiContainer");
+const emojiInput = document.getElementById("emoji-input")
+const addButton = document.getElementById("push-btn")
 
 myEmojis.forEach((element) => {
     const newSpan = document.createElement("span");
     newSpan.textContent = element;
     emojiContainer.appendChild(newSpan);
+});
+
+addButton.addEventListener("click", () => {
+    if (emojiInput.value !== "") {
+        myEmojis.push(emojiInput.value);
+        emojiInput.value = "";
+        console.log(myEmojis);
     }
-);
+});
